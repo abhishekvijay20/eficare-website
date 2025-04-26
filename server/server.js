10162b649ba7e218c6e8b7b6b59d2c92b5ac5417
abhishekvijay20 <abhishek.jangid8@gmail.com>
2025-04-27 02:56:36 +0530

    path of build corrected

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const path = require('path');
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // File upload handling
 const storage = multer.memoryStorage();
