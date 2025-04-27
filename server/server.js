b169dbc459080646263087e2421cc8552a393d60
abhishekvijay20 <abhishek.jangid8@gmail.com>
2025-04-27 06:32:16 +0530

    brevo port changed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const upload = multer({ storage });
 // Email configuration 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",  
-  port: 587,                    
+  port: 465,                    
   secure: false, 
   auth: {
     user: process.env.TRANSPORTER_LOGIN,
